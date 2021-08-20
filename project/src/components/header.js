e8017c5ae3cb0766a4f3f8c3ffd4abf3d9527f5e
Ubuntu <ubuntu@ip-172-31-12-37.ap-northeast-2.compute.internal>
2021-08-20 07:26:45 +0000

    update

--- project/src/components/header.js
+++ project/src/components/header.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import '/home/ubuntu/ict/project/src/header.css'
 
 class Header extends Component {
 
     render() {
         return (
-            <div>
-                <h3> 헤더!!!!!~~~~~~~~~~~ </h3>
-            </div>
+            <header className="header">
+
+
+
+                <div className="header_logo">
+                    오늘 뭐 먹지?
+                </div>
+
+
+                <ul className="header_menu_ul">
+
+                    <li> <Link to="/delivery"> About me </Link></li>
+                    <li> <Link to="/homemeal"> Albums </Link></li>
+                    <li> <Link to="/random"> Photos </Link></li>
+                </ul>
+
+
+            </header>
+
+
+
+
+
         );
     }
 }
