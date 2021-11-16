da0b6314a94c7d48fd920322cb1f4bb395248b2b
Ubuntu <ubuntu@ip-172-31-12-37.ap-northeast-2.compute.internal>
2021-11-16 16:38:10 +0000

    modify gitignore

--- privateProperty.js
+++ privateProperty.js
@@ -1,4 +1,4 @@
 exports.getIP = function () {
-    const privateProps = '3.35.17.24';
+    const privateProps = '3.35.17.24';  //IP
     return privateProps;
 }
--- privateProperty_client.js
+++ privateProperty_client.js
@@ -1,6 +1,6 @@
 export default function PrivateProperty() {
     const privateProps = {
-        IP: '3.35.17.24',
+        IP: '3.35.17.24',   //IP
     };
     return privateProps;
 }
